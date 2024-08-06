820253635357aa28a2623f5bb5b31aac4d2ed8f9
dart <follow@live.in>
2024-08-06 16:56:20 +0800

    提交代码。

--- src/NodeService.WebServer/Servers/PrimaryServer.cs
+++ src/NodeService.WebServer/Servers/PrimaryServer.cs
@@ -459,7 +459,7 @@ namespace NodeService.WebServer.Servers
 
             builder.Services.AddSingleton<INodeSessionService, NodeSessionService>();
             builder.Services.AddSingleton(new BatchQueue<NodeHeartBeatSessionMessage>(TimeSpan.FromSeconds(3), 2048));
-            builder.Services.AddSingleton(new BatchQueue<NodeStatusChangeRecordModel>(TimeSpan.FromHours(8), 1024*100));
+            builder.Services.AddSingleton(new BatchQueue<NodeStatusChangeRecordModel>(TimeSpan.FromSeconds(3), 1024));
 
             builder.Services.AddSingleton(new BatchQueue<DataQualityAlarmMessage>(TimeSpan.FromHours(1), 1024*100));
             builder.Services.AddSingleton(

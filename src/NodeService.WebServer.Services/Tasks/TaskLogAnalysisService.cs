108fe8339f7e772c1f83327254deeccfc97e4a46
dart <follow@live.in>
2024-08-19 05:32:30 +0800

    优化代码。

--- src/NodeService.WebServer.Services/Tasks/TaskLogAnalysisService.cs
+++ src/NodeService.WebServer.Services/Tasks/TaskLogAnalysisService.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using NodeService.Infrastructure.DataModels;
 using NodeService.Infrastructure.Logging;
 using NodeService.WebServer.Data.Repositories;
 using NodeService.WebServer.Services.Counters;
@@ -19,6 +20,7 @@ namespace NodeService.WebServer.Services.Tasks
         readonly ObjectCache _objectCache;
         readonly ExceptionCounter _exceptionCounter;
         readonly ApplicationRepositoryFactory<TaskProgressInfoModel> _taskProgressInfoRepoFactory;
+        readonly BatchQueue<TaskProgressEntry> _batchQueue;
 
         public TaskLogAnalysisService(
             ILogger<TaskLogAnalysisService> logger,
@@ -32,9 +34,108 @@ namespace NodeService.WebServer.Services.Tasks
             _objectCache = objectCache;
             _exceptionCounter = exceptionCounter;
             _taskProgressInfoRepoFactory = taskProgressInfoRepoFactory;
+            _batchQueue = new BatchQueue<TaskProgressEntry>();
         }
 
         protected override async Task ExecuteAsync(CancellationToken cancellationToken = default)
+        {
+            await Task.WhenAll(
+                PersistTaskProgressInfoAsync(cancellationToken),
+                AnalysisTaskLogAsync(cancellationToken));
+        }
+
+        private async Task PersistTaskProgressInfoAsync(CancellationToken cancellationToken = default)
+        {
+            await foreach (var entries in _batchQueue.ReceiveAllAsync(cancellationToken))
+            {
+                if (entries == null)
+                {
+                    continue;
+                }
+                foreach (var taskProgressEntryGroup in entries.GroupBy(static x => x.Id))
+                {
+                    var id = taskProgressEntryGroup.Key;
+                    if (id == null)
+                    {
+                        continue;
+                    }
+
+                    try
+                    {
+                        await using var taskProgressInfoRepo = await _taskProgressInfoRepoFactory.CreateRepositoryAsync(cancellationToken);
+                        var taskProgressInfo = await taskProgressInfoRepo.GetByIdAsync(id, cancellationToken);
+                        var addOrUpdate = false;
+                        if (taskProgressInfo == null)
+                        {
+                            taskProgressInfo = new TaskProgressInfoModel()
+                            {
+                                Id = id,
+                                Name = string.Empty,
+                                CreationDateTime = DateTime.UtcNow,
+                                ModifiedDateTime = DateTime.UtcNow,
+                            };
+                            addOrUpdate = true;
+                        }
+                        var changed = false;
+                        foreach (var taskProgressEntry in taskProgressEntryGroup)
+                        {
+                            if (taskProgressInfo.Value.Entries.Count == 0)
+                            {
+                                taskProgressInfo.Value.Entries.Add(taskProgressEntry);
+                                changed = true;
+                            }
+                            else
+                            {
+                                var found = false;
+                                for (int i = 0; i < taskProgressInfo.Entries.Count; i++)
+                                {
+                                    var entry = taskProgressInfo.Entries[i];
+                                    if (entry.TaskName == taskProgressEntry.TaskName)
+                                    {
+                                        if (entry.Progress != taskProgressEntry.Progress)
+                                        {
+                                            entry.Progress = taskProgressEntry.Progress;
+                                            taskProgressInfo.Entries[i] = entry;
+                                            changed = true;
+                                        }
+                                        found = true;
+                                        break;
+                                    }
+                                }
+                                if (!found)
+                                {
+                                    taskProgressInfo.Entries.Add(taskProgressEntry);
+                                    changed = true;
+                                }
+                            }
+                        }
+
+
+
+                        if (addOrUpdate)
+                        {
+                            await taskProgressInfoRepo.AddAsync(taskProgressInfo, cancellationToken);
+                        }
+                        else
+                        {
+                            if (changed)
+                            {
+                                await taskProgressInfoRepo.UpdateAsync(taskProgressInfo, cancellationToken);
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.LogError(ex.ToString());
+                        _exceptionCounter.AddOrUpdate(ex, id);
+                    }
+
+                }
+
+            }
+        }
+
+        private async Task AnalysisTaskLogAsync(CancellationToken cancellationToken)
         {
             while (!cancellationToken.IsCancellationRequested)
             {
@@ -52,7 +153,6 @@ namespace NodeService.WebServer.Services.Tasks
 
             }
         }
-
 
         private async ValueTask AnalysisAsync(TaskLogUnit taskLogUnit, CancellationToken cancellationToken = default)
         {
@@ -81,62 +181,8 @@ namespace NodeService.WebServer.Services.Tasks
                 {
                     return;
                 }
-                await using var taskProgressInfoRepo = await _taskProgressInfoRepoFactory.CreateRepositoryAsync(cancellationToken);
-                var taskProgressInfo = await taskProgressInfoRepo.GetByIdAsync(taskLogUnit.Id, cancellationToken);
-                var addOrUpdate = false;
-                if (taskProgressInfo == null)
-                {
-                    taskProgressInfo = new TaskProgressInfoModel()
-                    {
-                        Id = taskLogUnit.Id,
-                        Name = string.Empty,
-                        CreationDateTime = DateTime.UtcNow,
-                        ModifiedDateTime = DateTime.UtcNow,
-                    };
-                    addOrUpdate = true;
-                }
-                var changed = false;
-                if (taskProgressInfo.Value.Entries.Count == 0)
-                {
-                    taskProgressInfo.Value.Entries.Add(taskProgressEntry);
-                    changed = true;
-                }
-                else
-                {
-                    var found = false;
-                    for (int i = 0; i < taskProgressInfo.Entries.Count; i++)
-                    {
-                        var entry = taskProgressInfo.Entries[i];
-                        if (entry.TaskName == taskProgressEntry.TaskName)
-                        {
-                            if (entry.Progress != taskProgressEntry.Progress)
-                            {
-                                entry.Progress = taskProgressEntry.Progress;
-                                taskProgressInfo.Entries[i] = entry;
-                                changed = true;
-                            }
-                            found = true;
-                            break;
-                        }
-                    }
-                    if (!found)
-                    {
-                        taskProgressInfo.Entries.Add(taskProgressEntry);
-                        changed = true;
-                    }
-                }
-                if (addOrUpdate)
-                {
-                    await taskProgressInfoRepo.AddAsync(taskProgressInfo, cancellationToken);
-                }
-                else
-                {
-                    if (changed)
-                    {
-                        await taskProgressInfoRepo.UpdateAsync(taskProgressInfo, cancellationToken);
-                    }
-                }
-
+                taskProgressEntry.Id = taskLogUnit.Id;
+                await _batchQueue.SendAsync(taskProgressEntry, cancellationToken);
             }
             catch (Exception ex)
             {
